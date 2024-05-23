c9c2f2ca24c0ffef32c42b15f8f30928e7587311
kamgang william <kamgangwilliam2021@gmail.com>
2024-05-23 09:31:48 +0200

    manque de couleur

---
+++ app/(common-layout)/(agent)/agent-details-review/page.jsx
@@ -0,0 +1,99 @@
+import { HandThumbDownIcon, HandThumbUpIcon, HeartIcon, StarIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+const page = () => {
+  const comments = [
+    {
+      name: "William",
+      job: "DevOps",
+      date: "23.10.2024",
+      hour: "18h15",
+      icon: "star",
+      comment: "Excellent chauffeur ! Christian était très professionnel et ponctuel. J'ai passé un trajet très agréable avec lui. Je le recommande vivement !"
+    },
+    {
+      name: "Lushai",
+      job: "Business Analyst",
+      date: "23.10.2024",
+      hour: "18h15",
+      icon: "heart",
+      comment: "Christian était un chauffeur sympathique et courtois. Il a pris le temps de discuter pendant le trajet et m'a fait me sentir à l'aise. Je le choisirai à nouveau pour mes prochains déplacements."
+    },
+    {
+      name: "Josée",
+      job: "Entrepreneure",
+      date: "23.10.2024",
+      hour: "18h15",
+      icon: "hand_up",
+      comment: "Je recommande fortement Christian en tant que chauffeur. Il était très professionnel et attentionné. Le trajet s'est déroulé sans problème et j'ai beaucoup apprécié sa compagnie."
+    },
+    {
+      name: "Cedrick",
+      job: "Ingénieur logiciel",
+      date: "23.10.2024",
+      hour: "18h15",
+      icon: "hand_down",
+      comment: "Il était en retard "
+    },
+    {
+      name: "Foueguim",
+      job: "Webmaster",
+      date: "23.10.2024",
+      hour: "18h15",
+      icon: "heart",
+      comment: "Christian était un excellent chauffeur. Il était amical, serviable et a rendu le trajet très agréable. Je le recommande à tous ceux qui recherchent un chauffeur fiable."
+    }
+  ]
+  
+  return (
+    <div className="col-span-12">
+      <div className="p-3 bg-white rounded-2xl mb-8">
+        <div className="bg-white rounded-2xl">
+          {
+            comments.map((comment)=>{
+              return(
+                <div className="bg-[var(--bg-1)] rounded-2xl p-3 sm:p-4 lg:p-6 mb-2">
+                  <div className="flex items-center flex-wrap justify-between gap-4 ">
+                    <div className="flex gap-5 items-center">
+                      <div className="flex-grow">
+                        <h5 className="mb-1 font-semibold"> {comment.name} | {comment.job}</h5>
+                      </div>
+                      <div>
+                        {
+                          function(){
+                            if(comment.icon == "star") return(<StarIcon className="w-5 h-5 text-[var(--tertiary)]" />)
+                            if(comment.icon == "heart") return(<HeartIcon className="w-5 h-5 text-red-500" />)
+                            if(comment.icon == "hand_up") return(<HandThumbUpIcon className="w-5 h-5 text-blue-500" />)
+                            if(comment.icon == "hand_down") return(<HandThumbDownIcon className="w-5 h-5 text-gray-900" />)
+                          }()
+                        }
+                      </div>
+                    </div>
+                    
+                    <div className="text-sm-end">
+                      <p className="mb-1"> {comment.date} à {comment.hour}</p>
+                    </div>
+                  </div>
+                  <div className="border border-dashed my-2"></div>
+                  <p className="mb-0 clr-neutral-500">
+                    {comment.comment}
+                  </p>
+                </div>
+              )
+            })
+          }
+          
+          
+          <Link
+            href="#"
+            className="featured-tab link font-semibold clr-primary-400 inline-block py-3 px-6 bg-[var(--primary-light)] hover:bg-primary hover:text-white rounded-full active">
+            Voir tous les commentaires
+          </Link>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default page;
---
+++ app/(common-layout)/(agent)/layout.jsx
@@ -0,0 +1,150 @@
+"use client";
+import axios from 'axios';
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from 'react';
+import { HeartIcon, StarIcon, HandThumbUpIcon, HandThumbDownIcon } from "@heroicons/react/24/solid";
+
+export default function RootLayout({children}) {
+  let URL = "http://localhost:8086"
+  let iconsNumber = {
+    starts: 1,
+    hearts: 2,
+    handUp: 1,
+    handDown: 1
+  }
+
+  let [disponible, setDisponible] = useState(true)
+  useEffect(() => {
+    const driverId = "IDENTIFIANT PASSE EN GET"
+    axios.get(URL+"/isDisponible?driverId="+driverId)
+    .then(response => {
+      dispo = response.disponible === "true"
+      setDisponible(dispo)
+    })
+    .catch(error => console.error(error))
+  }, []);
+
+  return (
+    <div className="py-[30px] lg:py-[60px] bg-[var(--bg-2)] px-3">
+      <div className="container">
+        <div className="grid grid-cols-12 gap-4 lg:gap-6">
+          <div className="col-span-12 xl:col-span-4">
+            <div className="bg-white rounded-2xl p-3 sm:p-4 lg:py-8 lg:px-6 mb-6">
+              <div className="w-32 h-32 border ovenflow-hidden border-[var(--primary)] rounded-full bg-white p-4 grid place-content-center relative mx-auto mb-10">
+                <Image
+                  width={130}
+                  height={130}
+                  src="/img/chauffeur.png"
+                  alt="image"
+                  className="rounded-full w-full h-full"
+                />
+              </div>
+              <h4 className="text-center text-2xl font-semibold mb-4">
+                M. Christian
+              </h4>
+              <p className="mb-0">
+                Contact: <span className="text-primary">6******** | Melen Yaounde</span>
+              </p>
+              <ul className="flex items-center justify-center flex-wrap mb-7">
+                <li>
+                  <p className="mb-0">
+                    Mail: <span className="text-primary">***@gmail.com</span>
+                  </p>
+                </li>
+                <div div className='flex min-w-full justify-evenly my-3'>
+                  <div className='flex items-center'>
+                    <StarIcon className="w-5 h-5 text-[var(--tertiary)]" /> <span className='ml-2'>{iconsNumber.starts}</span>
+                  </div>
+                  <div className='flex items-center'>
+                    <HeartIcon className="w-5 h-5 text-red-500" /> <span className='ml-2'>{iconsNumber.hearts}</span>
+                  </div>
+                  <div className='flex items-center'>
+                    <HandThumbUpIcon className="w-5 h-5 text-blue-500" /> <span className='ml-2'>{iconsNumber.handUp}</span>
+                  </div>
+                  <div className='flex items-center'>
+                    <HandThumbDownIcon className="w-5 h-5 text-gray-900" /> <span className='ml-2'>{iconsNumber.handDown}</span>
+                  </div>
+                </div>
+                <li className="flex gap-4">
+                  {disponible?
+                    <div className='flex gap-4'>
+                      <Link href="#"
+                        className="inline-flex items-center gap-2 p-2 rounded-full bg-primary text-white :bg-primary-400 hover:text-white font-medium mt-2">
+                        <span className="inline-block"> Contacter </span>
+                      </Link>
+                      <div href="#"
+                        className="inline-flex items-center gap-2 p-2 rounded-full bg-[green] text-white :bg-primary-400 hover:text-white font-medium mt-2">
+                        <span className="inline-block"> Disponible </span>
+                      </div>
+                    </div>
+                    :
+                    <div href="#"
+                      className="inline-flex items-center gap-2 p-2 rounded-full bg-[red] text-white :bg-primary-400 hover:text-white font-medium mt-2">
+                      <span className="inline-block"> inDisponible </span>
+                    </div>
+                  }
+                  
+                </li>
+              </ul>
+              
+              <div className="border border-dashed my-7"></div>
+              <ul className="flex flex-col gap-4 mb-10 max-text-30 mx-auto">
+                <li>
+                  <div className="items-center gap-2">
+                    {/* <CalendarDaysIcon className="w-5 h-5 text-primary" /> */}
+                    <div className="block text-1xl font-semibold clr-neutral-600 mb-4">
+                      Formation
+                    </div>
+                    <div className="mb-0"> 1990 Permis B à AUTO-ECOLE KASAP</div>
+                  </div>
+                </li>
+                <div className="border border-dashed my-2"></div>
+                <li>
+                  <div className="items-center gap-2">
+                    {/* <ChatBubbleLeftEllipsisIcon className="w-5 h-5 text-[var(--secondary)]" />
+                    <p className="mb-0"> Response rate - 100% </p> */}
+                    <div className="block text-1xl font-semibold clr-neutral-600 mb-4">
+                      Expérience professionelle
+                    </div>
+                    <div className="mb-0">* De mars 2018 à juin 2020: conducteur indépendant</div>
+                    <div className="mb-0">* De juin 2020 à aujourd'hui: conducteur chez ******** <b className="text-color-green">aucun accident</b></div>
+                  </div>
+                </li>
+              </ul>
+
+              <div className="border border-dashed my-2"></div>
+              <ul className="flex flex-col gap-4">
+                <li className="flex items-center justify-between flex-wrap">
+                  <p className="mb-0">Convivialité</p>
+                  <p className="mb-0 font-medium">Excellent</p>
+                </li>
+                <li className="flex items-center justify-between flex-wrap">
+                  <p className="mb-0">Politesse</p>
+                  <p className="mb-0 font-medium">Moyen</p>
+                </li>
+                <li className="flex items-center justify-between flex-wrap">
+                  <p className="mb-0">Patience</p>
+                  <p className="mb-0 font-medium">Excellent</p>
+                </li>
+              </ul>
+              <div className="border border-dashed my-6"></div>
+              <span className="block text-2xl font-semibold clr-neutral-600 mb-4">
+                Details
+              </span>
+              <p className="mb-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae perspiciatis necessitatibus rem, fugiat dolores obcaecati ipsam nostrum sint ea veniam commodi et, excepturi sunt quidem, possimus sequi eum dicta.
+              </p>
+            
+            </div>
+          </div>
+          <div className="col-span-12 xl:col-span-8">
+            <div className="grid grid-cols-12 gap-4 align-items-start">
+              {children}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
